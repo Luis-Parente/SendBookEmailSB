b533d5c722090cd8347859b8aebff20217c665b9
Luis Parente <lgsp25@hotmail.com>
2025-07-15 22:09:53 -0300

    Project created

---
+++ src/main/java/com/devsuperior/SendBookEmailSB/SendBookEmailSbApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SendBookEmailSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBookEmailSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendBookEmailSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/SendBookEmailSB/SendBookEmailSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SendBookEmailSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
